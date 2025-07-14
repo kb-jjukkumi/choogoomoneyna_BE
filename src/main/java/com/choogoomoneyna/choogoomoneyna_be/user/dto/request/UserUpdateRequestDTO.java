39f083076d760232bf80f1cebc2bde375003ba29
이학준 <dlgkrwns213@naver.com>
2025-07-14 18:09:19 +0900

    feat: UserUpdateRequestDTO 선언

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/dto/request/UserUpdateRequestDTO.java
@@ -0,0 +1,18 @@
+package com.choogoomoneyna.choogoomoneyna_be.user.dto.request;
+
+import com.choogoomoneyna.choogoomoneyna_be.user.dto.ChoogooMi;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserUpdateRequestDTO {
+    private String nickname;
+    private MultipartFile profileImage;
+    private ChoogooMi choogooMi;
+}
