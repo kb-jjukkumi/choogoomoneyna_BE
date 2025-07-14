8fcb47cd1f4b775cfb2ded630d594a5ba7953aa7
이학준 <dlgkrwns213@naver.com>
2025-07-14 14:27:29 +0900

    feat: UserDTO 작성

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/dto/UserDTO.java
@@ -0,0 +1,24 @@
+package com.choogoomoneyna.choogoomoneyna_be.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDTO {
+    private String email;
+    private String password;
+    private String nickname;
+    private Date regDate;  // 등록일
+    private Date updateDate;  // 수정일
+    private MultipartFile profileImage;
+    private ChoogooMi choogooMi;  // 추구미 유형
+    private Integer[] choogooMiScores;  // 각 추구미 점수 -> 5개
+}
