7c0143c0171fd1e8aaff68807ceea7b03719b77f
이학준 <dlgkrwns213@naver.com>
2025-07-14 16:46:27 +0900

    feat: vo <-> dto 변환 코드 작성 및 dto를 그에 맞게 수정

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/service/UserConverter.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/service/UserConverter.java
@@ -1,4 +1,42 @@
 package com.choogoomoneyna.choogoomoneyna_be.user.service;
 
-public class UserConveter {
+import com.choogoomoneyna.choogoomoneyna_be.user.dto.ChoogooMi;
+import com.choogoomoneyna.choogoomoneyna_be.user.dto.UserDTO;
+import com.choogoomoneyna.choogoomoneyna_be.user.vo.UserVO;
+
+public class UserConverter {
+
+    // DTO -> VO
+    public static UserVO dtoToVo(UserDTO dto) {
+        if (dto == null)
+            return null;
+
+        return UserVO.builder()
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .nickname(dto.getNickname())
+                .profileImageUrl(dto.getProfileImageUrl())
+                .regDate(dto.getRegDate())
+                .updateDate(dto.getUpdateDate())
+                .choogooMi(dto.getChoogooMi().name())
+                .build();
+    }
+
+    // VO -> DTO
+    public static UserDTO voToDto(UserVO vo) {
+        if (vo == null)
+            return null;
+
+        UserDTO dto = new UserDTO();
+        dto.setEmail(vo.getEmail());
+        dto.setPassword(vo.getPassword());
+        dto.setNickname(vo.getNickname());
+        dto.setRegDate(vo.getRegDate());
+        dto.setUpdateDate(vo.getUpdateDate());
+        dto.setProfileImageUrl(vo.getProfileImageUrl());
+        dto.setProfileImage(null);
+        dto.setChoogooMi(ChoogooMi.valueOf(vo.getChoogooMi()));
+
+        return dto;
+    }
 }
