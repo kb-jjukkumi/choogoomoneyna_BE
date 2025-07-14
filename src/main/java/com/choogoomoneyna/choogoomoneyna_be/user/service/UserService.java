fbdc196808fe96f9864d7ff6d5fae4eac7040b08
이학준 <dlgkrwns213@naver.com>
2025-07-14 17:57:07 +0900

    feat: LoginType enum class 설정

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.choogoomoneyna.choogoomoneyna_be.user.service;
+
+import com.choogoomoneyna.choogoomoneyna_be.user.dto.UserJoinDTO;
+
+public interface UserService {
+    void registerUser(UserJoinDTO dto);
+
+
+}
