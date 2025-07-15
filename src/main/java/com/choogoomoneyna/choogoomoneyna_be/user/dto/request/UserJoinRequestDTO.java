8fbd8306bdb3f4c8bbf7c5493c8761a317d73838
이학준 <dlgkrwns213@naver.com>
2025-07-15 19:48:49 +0900

    feat: NotBlank, NotNull 어노테이션 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/dto/request/UserJoinRequestDTO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/dto/request/UserJoinRequestDTO.java
@@ -1,20 +1,30 @@
 package com.choogoomoneyna.choogoomoneyna_be.user.dto.request;
 
 import com.choogoomoneyna.choogoomoneyna_be.user.dto.ChoogooMi;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserJoinRequestDTO {
+
+    @NotBlank(message = "이메일은 필수 입니다.")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입니다.")
     private String password;
+
+    @NotBlank(message = "닉네임은 필수로 설정해야 합니다")
     private String nickname;
+
     private MultipartFile profileImage;
+
+    @NotNull(message = "추구미는 필수로 설정해야 합니다")
     private ChoogooMi choogooMi;
 }
