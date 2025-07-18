305dfb76d701ab787f3b960946e915f4cd18d629
이학준 <dlgkrwns213@naver.com>
2025-07-18 10:20:49 +0900

    feat: emailAuthRequestDTO 생성

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/auth/email/dto/EmailAuthRequestDTO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/auth/email/dto/EmailAuthRequestDTO.java
@@ -1,4 +1,10 @@
 package com.choogoomoneyna.choogoomoneyna_be.auth.email.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class EmailAuthRequestDTO {
+    private String email;
 }
