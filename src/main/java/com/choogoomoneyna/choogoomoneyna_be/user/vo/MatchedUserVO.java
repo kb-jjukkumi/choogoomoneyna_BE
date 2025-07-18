bff8d8987790eb14bcf3b2932e42fbeb2856e5d2
이학준 <dlgkrwns213@naver.com>
2025-07-18 14:47:15 +0900

    feat: MatchedUserVO 생성

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/vo/MatchedUserVO.java
@@ -0,0 +1,15 @@
+package com.choogoomoneyna.choogoomoneyna_be.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MatchedUserVO {
+
+    private Long id;
+    private String nickname;
+    private String profileImageUrl;
+}
