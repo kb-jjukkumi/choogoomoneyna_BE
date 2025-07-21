1d9e56c3c8ff384a2fa84b46689604b022e015bd
이학준 <dlgkrwns213@naver.com>
2025-07-21 10:11:39 +0900

    feat: MatchingMissionResultVO 선언

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/vo/MatchingMissionResultVO.java
@@ -0,0 +1,9 @@
+package com.choogoomoneyna.choogoomoneyna_be.matching.vo;
+
+public class MatchingMissionResultVO {
+    private int id;
+    private int matchingId;
+    private int missionId;
+    private long userId;
+    private int resultScore;
+}
