59624025935908d666cc3bcee941f36a6ec0862c
권채린 <lin1041840@gmail.com>
2025-07-21 10:24:55 +0900

    feat: codef AccountVo

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/codef/vo/AccountVO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/codef/vo/AccountVO.java
@@ -1,4 +1,16 @@
 package com.choogoomoneyna.choogoomoneyna_be.account.codef.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class AccountVO {
+    private String accountNum;
+    private Long userId;
+    private String bankId;
+    private String accountName;
+    private int accountBalance;
 }
