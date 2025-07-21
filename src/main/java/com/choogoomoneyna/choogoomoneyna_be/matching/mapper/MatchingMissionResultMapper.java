a12f3047ea58ef1ae05b0004fb3145ec60afddcb
이학준 <dlgkrwns213@naver.com>
2025-07-21 16:37:31 +0900

    feat: MatchingMissionResultMapper interface 작성

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/mapper/MatchingMissionResultMapper.java
@@ -0,0 +1,12 @@
+package com.choogoomoneyna.choogoomoneyna_be.matching.mapper;
+
+import com.choogoomoneyna.choogoomoneyna_be.matching.vo.MatchingMissionResultVO;
+
+import java.util.List;
+
+public interface MatchingMissionResultMapper {
+
+    void insertAll(List<MatchingMissionResultVO> matchingMissionResultVOList);
+
+    List<MatchingMissionResultVO> findMatchingMissionByUserId(Long UserId);
+}
