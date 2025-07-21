57fe5d3e6e2fd65a966e4ab493e70768be7b7429
권채린 <lin1041840@gmail.com>
2025-07-21 17:37:14 +0900

    feat: 거래내역 요청,응답 dto 작성

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/codef/dto/TransactionRequestDto.java
@@ -0,0 +1,14 @@
+package com.choogoomoneyna.choogoomoneyna_be.account.codef.dto;
+
+import lombok.Data;
+
+@Data
+public class TransactionRequestDto {
+        private String connectedId;  // 커넥티드아이디
+        private String accountNum;   // 계좌번호
+        private String bankId;       // 은행 기관코드
+        private String startDate;    // 조회 시작일자 (yyyyMMdd)
+        private String endDate;      // 조회 종료일자 (yyyyMMdd)
+        private Integer offset;      // 추후 페이징
+        private Integer limit;       // 추후 페이징
+}
---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/codef/dto/TransactionResponseDto.java
@@ -0,0 +1,26 @@
+package com.choogoomoneyna.choogoomoneyna_be.account.codef.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TransactionResponseDto {
+    private String accountNum;        // 계좌번호
+    private List<trItem> transactionList;
+
+    @Data
+    public static class trItem {
+        private Long transactionId;       // 거래 고유 ID
+        private String trDate;            // 거래일 (yyyy-MM-dd 형식)
+        private int trTime;               // 거래시간 (HHmmss 정수)
+        private long trAccountOut;        // 출금액
+        private long trAccountIn;         // 입금액
+        private long trAfterBalance;      // 거래 후 잔액
+        private String trDesc1;           // 보내는 분,받는 분
+        private String trDesc2;           // 메모
+        private String trDesc3;           // 적요
+        private String trDesc4;           // 거래점
+    }
+
+}
