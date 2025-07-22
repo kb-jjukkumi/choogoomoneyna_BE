3c30250632ca6bfd95c9db344e7c2133e50df8cd
권채린 <lin1041840@gmail.com>
2025-07-22 17:59:54 +0900

    feat: AccountUpdateRequestDto 생성

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/codef/dto/AccountUpdateRequestDto.java
@@ -0,0 +1,11 @@
+package com.choogoomoneyna.choogoomoneyna_be.account.codef.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountUpdateRequestDto {
+    private String bankId;
+    private String accountNum;
+}
+
+
