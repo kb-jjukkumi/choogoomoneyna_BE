eca1ced6ca3de05f87038adf32ba075595c1b903
이학준 <dlgkrwns213@naver.com>
2025-07-22 10:20:10 +0900

    feat: RoundInof VO 선언

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/vo/RoundInfoVO.java
@@ -0,0 +1,18 @@
+package com.choogoomoneyna.choogoomoneyna_be.matching.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class RoundInfoVO {
+    private Integer roundNumber;
+    private Date StartDate;
+    private Date EndDate;
+}
