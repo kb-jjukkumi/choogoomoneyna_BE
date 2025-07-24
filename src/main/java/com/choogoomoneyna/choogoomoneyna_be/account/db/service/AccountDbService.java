aede6c40d7b76cb80aef8b0e6ff5de971aff8142
권채린 <lin1041840@gmail.com>
2025-07-24 11:38:57 +0900

    feat: getAllTransactions() 메서드 작성

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/db/service/AccountDbService.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/db/service/AccountDbService.java
@@ -1,6 +1,9 @@
 package com.choogoomoneyna.choogoomoneyna_be.account.db.service;
 
 import com.choogoomoneyna.choogoomoneyna_be.account.codef.dto.AccountResponseDto;
+import com.choogoomoneyna.choogoomoneyna_be.account.codef.dto.TransactionRequestDto;
+import com.choogoomoneyna.choogoomoneyna_be.account.codef.dto.TransactionResponseDto;
+import com.choogoomoneyna.choogoomoneyna_be.account.db.dto.TransactionItemDto;
 
 import java.util.List;
 
@@ -12,4 +15,12 @@ public interface AccountDbService {
      * @return 유저의 계좌 목록 반환, 없을 경우는 null
      */
     List<AccountResponseDto> getAllAccounts(Long userId);
+
+    /**
+     * account(계좌번호)로 등록된 거래내역을 모두 조회합니다.
+     * @param account 계좌번호
+     * @return 해당 계좌번호로 등록된 모든 거래내역
+     */
+    List<TransactionItemDto> getAllTransactions(String account);
+
 }
--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/db/service/AccountDbServiceImpl.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/account/db/service/AccountDbServiceImpl.java
@@ -1,12 +1,15 @@
 package com.choogoomoneyna.choogoomoneyna_be.account.db.service;
 
 import com.choogoomoneyna.choogoomoneyna_be.account.codef.dto.AccountResponseDto;
+import com.choogoomoneyna.choogoomoneyna_be.account.codef.vo.TransactionVO;
+import com.choogoomoneyna.choogoomoneyna_be.account.db.dto.TransactionItemDto;
 import com.choogoomoneyna.choogoomoneyna_be.account.db.mapper.AccountMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,9 +17,21 @@ import java.util.List;
 public class AccountDbServiceImpl implements AccountDbService {
 
     private final AccountMapper accountMapper;
+    //private final TransactionConverter transactionConverter;
 
     @Override
     public List<AccountResponseDto> getAllAccounts(Long userId) {
         return accountMapper.findByUserId(userId);
     }
+
+    @Override
+    public List<TransactionItemDto> getAllTransactions(String account) {
+        List<TransactionVO> voList = accountMapper.findAllTransactionsVo(account);
+
+        return voList.stream().map(vo -> {
+            TransactionItemDto dto = TransactionConverter.voToItemDto(vo);
+            return dto;
+        }).collect(Collectors.toList());
+    }
+
 }
