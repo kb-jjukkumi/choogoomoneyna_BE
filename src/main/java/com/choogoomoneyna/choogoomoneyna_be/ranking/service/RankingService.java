7b39331fb43cccd3da5d84dd7907100edac93418
이학준 <dlgkrwns213@naver.com>
2025-07-25 13:44:37 +0900

    feat: RankingService에서 batchupdate 기능 및 랭킹 db 에 추가하는 기능 선언

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/service/RankingService.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/service/RankingService.java
@@ -1,16 +1,23 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.service;
 
+import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingUpdateVO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingVO;
+
+import java.util.List;
 
 public interface RankingService {
 
     void createRanking(RankingVO rankingVO);
 
-    void updateCurrentRankingByUserId(Long userId, int currentRank);
+    void updateCurrentRankingByUserId(Long userId, int currentRanking);
 
     RankingVO findRankingByUserId(Long userId);
 
     Integer getCurrentRanking(Long userId);
 
     void rolloverWeeklyRankings();
+
+    void batchUpdateCurrentRanks(List<RankingUpdateVO> rankingList);
+
+    void updateRanking();
 }
