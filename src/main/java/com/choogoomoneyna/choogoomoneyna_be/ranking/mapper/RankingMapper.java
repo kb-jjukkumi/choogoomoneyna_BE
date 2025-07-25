0705747b0e0f845e66f2d87c24ec59181e796278
이학준 <dlgkrwns213@naver.com>
2025-07-25 13:45:07 +0900

    feat: RankingMapper에서 batchupdate 기능 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/mapper/RankingMapper.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/mapper/RankingMapper.java
@@ -1,8 +1,11 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.mapper;
 
+import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingUpdateVO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface RankingMapper {
@@ -10,11 +13,13 @@ public interface RankingMapper {
     void insertRanking(RankingVO rankingVO);
 
     void updateCurrentRankingByUserId(@Param("userId") Long userId,
-                                      @Param("currentRank") Integer currentRank);
+                                      @Param("currentRanking") Integer currentRanking);
 
     RankingVO findRankingByUserId(Long userId);
 
     Integer getCurrentRanking(Long userId);
 
     void rolloverWeeklyRankings();
+
+    void batchUpdateCurrentRanks(@Param("rankingList") List<RankingUpdateVO> rankingList);
 }
