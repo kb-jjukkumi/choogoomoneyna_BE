027c826cc8fc8fe3678f564861c00556d90a0fe9
이학준 <dlgkrwns213@naver.com>
2025-07-29 14:35:12 +0900

    feat: 랭킹 업데이트 및 랭킹 가져오기 api 추가

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/controller/RankingController.java
@@ -0,0 +1,39 @@
+package com.choogoomoneyna.choogoomoneyna_be.ranking.controller;
+
+import com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response.RankingResponseDTO;
+import com.choogoomoneyna.choogoomoneyna_be.ranking.service.RankingConverter;
+import com.choogoomoneyna.choogoomoneyna_be.ranking.service.RankingUpdateServiceImpl;
+import com.choogoomoneyna.choogoomoneyna_be.ranking.service.RankingUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ranking")
+public class RankingController {
+
+    private final RankingUserService rankingUserService;
+    private final RankingUpdateServiceImpl rankingUpdateService;
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateRanking() {
+        rankingUpdateService.updateRanking();
+        return ResponseEntity.ok("Ranking has been updated");
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getRankingList() {
+        List<RankingResponseDTO> rankingList = rankingUserService.findLatestRankingUserPerUser()
+                .stream()
+                .map(RankingConverter::toRankingResponseDTO)
+                .toList();
+
+        return ResponseEntity.ok(rankingList);
+    }
+}
