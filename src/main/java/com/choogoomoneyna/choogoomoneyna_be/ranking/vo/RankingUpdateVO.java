fb8faf877fc0bc8fa3baa428fe7997cba64ab6c3
이학준 <dlgkrwns213@naver.com>
2025-07-29 11:33:09 +0900

    feat: RankingUpdateVO 에 rankingId, updateDate 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/vo/RankingUpdateVO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/vo/RankingUpdateVO.java
@@ -1,11 +1,19 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class RankingUpdateVO {
+    private Long rankingId;
     private Long userId;
     private Integer currentRanking;
+    private Date updateDate;
 }
