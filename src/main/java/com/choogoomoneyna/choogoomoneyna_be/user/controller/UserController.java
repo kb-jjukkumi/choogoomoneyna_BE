5a665dbad72471905392a09f747cc7070cc4b431
이학준 <dlgkrwns213@naver.com>
2025-07-30 17:53:07 +0900

    feat: main-profile 확인 용 API 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/controller/UserController.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/controller/UserController.java
@@ -1,7 +1,14 @@
 package com.choogoomoneyna.choogoomoneyna_be.user.controller;
 
+import com.choogoomoneyna.choogoomoneyna_be.auth.jwt.CustomUserDetails;
+import com.choogoomoneyna.choogoomoneyna_be.matching.service.RoundInfoService;
+import com.choogoomoneyna.choogoomoneyna_be.user.dto.response.UserMainResponseDTO;
+import com.choogoomoneyna.choogoomoneyna_be.user.service.UserConverter;
+import com.choogoomoneyna.choogoomoneyna_be.user.service.UserMainInfoService;
 import com.choogoomoneyna.choogoomoneyna_be.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,6 +16,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final RoundInfoService roundInfoService;
     private final UserService userService;
+    private final UserMainInfoService userMainInfoService;
 
+    @GetMapping("/main-profile")
+    ResponseEntity<UserMainResponseDTO> getMainProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getId();
+        Integer roundNumber = roundInfoService.getRoundNumber();
+
+        UserMainResponseDTO dto;
+        try {
+            dto = UserConverter.toUserMainResponseDTO(
+                    userMainInfoService.findUserMainResponseByUserIdAndRoundNumber(userId, roundNumber)
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return ResponseEntity.ok(dto);
+    }
 }
