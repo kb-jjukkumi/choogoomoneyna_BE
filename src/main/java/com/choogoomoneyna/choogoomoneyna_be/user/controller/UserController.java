f2b5c01c1fb350c0b2bb5f33257a396a81137a7d
이학준 <dlgkrwns213@naver.com>
2025-07-30 19:55:17 +0900

    feat: try-catch 문 삭제

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/controller/UserController.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.choogoomoneyna.choogoomoneyna_be.user.controller;
 
 import com.choogoomoneyna.choogoomoneyna_be.auth.jwt.CustomUserDetails;
 import com.choogoomoneyna.choogoomoneyna_be.matching.service.RoundInfoService;
+import com.choogoomoneyna.choogoomoneyna_be.score.service.ScoreService;
 import com.choogoomoneyna.choogoomoneyna_be.user.dto.response.UserMainResponseDTO;
 import com.choogoomoneyna.choogoomoneyna_be.user.service.UserConverter;
 import com.choogoomoneyna.choogoomoneyna_be.user.service.UserMainInfoService;
@@ -19,21 +20,19 @@ public class UserController {
     private final RoundInfoService roundInfoService;
     private final UserService userService;
     private final UserMainInfoService userMainInfoService;
+    private final ScoreService scoreService;
 
     @GetMapping("/main-profile")
     ResponseEntity<UserMainResponseDTO> getMainProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Long userId = userDetails.getId();
         Integer roundNumber = roundInfoService.getRoundNumber();
 
-        UserMainResponseDTO dto;
-        try {
-            dto = UserConverter.toUserMainResponseDTO(
+        UserMainResponseDTO dto = UserConverter.toUserMainResponseDTO(
                     userMainInfoService.findUserMainResponseByUserIdAndRoundNumber(userId, roundNumber)
             );
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().body(null);
-        }
+
+        // 보여줬으면 다시 false 로 변경
+        scoreService.updateIsLevelUpByUserId(userId, false);
 
         return ResponseEntity.ok(dto);
     }
