95e1b16a5322fa68ccfb241792f705dd29a52069
이학준 <dlgkrwns213@naver.com>
2025-07-31 14:45:10 +0900

    feat: SurveyResponseService 구현

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/service/SurveyResponseServiceImpl.java
@@ -0,0 +1,25 @@
+package com.choogoomoneyna.choogoomoneyna_be.survey.service;
+
+import com.choogoomoneyna.choogoomoneyna_be.survey.mapper.SurveyResponseMapper;
+import com.choogoomoneyna.choogoomoneyna_be.survey.vo.SurveyResponseVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SurveyResponseServiceImpl implements SurveyResponseService {
+
+    private final SurveyResponseMapper surveyResponseMapper;
+
+    @Override
+    public void insertSurveyResponse(SurveyResponseVO surveyResponseVO) {
+        surveyResponseMapper.insertSurveyResponse(surveyResponseVO);
+    }
+
+    @Override
+    public List<SurveyResponseVO> findSurveyResponseByUserId(Long userId) {
+        return surveyResponseMapper.findSurveyResponseByUserId(userId);
+    }
+}
