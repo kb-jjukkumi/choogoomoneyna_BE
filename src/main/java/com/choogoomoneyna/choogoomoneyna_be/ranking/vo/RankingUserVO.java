7d75578a98c3ebdf24c1d06a5deec8204be83d27
이학준 <dlgkrwns213@naver.com>
2025-07-31 17:14:30 +0900

    feat: RankingUserVO 에 추구미 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/vo/RankingUserVO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/vo/RankingUserVO.java
@@ -1,11 +1,9 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.vo;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -15,4 +13,5 @@ public class RankingUserVO {
     private String nickname;
     private Integer ranking;
     private Integer score;
+    private String ChoogooMi;
 }
