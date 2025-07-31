b754fcbb26d787c849c7a563baa1d947c90f6813
이학준 <dlgkrwns213@naver.com>
2025-07-31 17:37:09 +0900

    feat: RankingUserChangeVO 에서 RankingChnageResponseDTO 로 변환코드 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/service/RankingConverter.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/service/RankingConverter.java
@@ -1,7 +1,9 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.service;
 
+import com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response.RankingChangeResponseDTO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response.RankingHistoryDTO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response.RankingResponseDTO;
+import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingUserChangeVO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingUserVO;
 import com.choogoomoneyna.choogoomoneyna_be.ranking.vo.RankingVO;
 import com.choogoomoneyna.choogoomoneyna_be.user.enums.ChoogooMi;
@@ -14,6 +16,16 @@ public class RankingConverter {
                 .score(rankingUserVO.getScore())
                 .ranking(rankingUserVO.getRanking())
                 .choogooMi(ChoogooMi.valueOf(rankingUserVO.getChoogooMi()))
+                .build();
+    }
+
+    public static RankingChangeResponseDTO toRankingChangeResponseDTO(RankingUserChangeVO rankingUserChangeVO) {
+        return RankingChangeResponseDTO.builder()
+                .userNickname(rankingUserChangeVO.getNickname())
+                .score(rankingUserChangeVO.getScore())
+                .ranking(rankingUserChangeVO.getRanking())
+                .beforeRanking(rankingUserChangeVO.getBeforeRanking())
+                .choogooMi(ChoogooMi.valueOf(rankingUserChangeVO.getChoogooMi()))
                 .build();
     }
 
