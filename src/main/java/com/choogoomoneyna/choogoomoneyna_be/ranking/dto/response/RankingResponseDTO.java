334bc5f11dc855c160d02a9864d8526d9bf16b03
이학준 <dlgkrwns213@naver.com>
2025-07-31 17:14:41 +0900

    feat: RankingResponseDTO 에 추구미 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/dto/response/RankingResponseDTO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/dto/response/RankingResponseDTO.java
@@ -1,5 +1,6 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response;
 
+import com.choogoomoneyna.choogoomoneyna_be.user.enums.ChoogooMi;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +10,5 @@ public class RankingResponseDTO {
     private String userNickname;
     private Integer ranking;
     private Integer score;
+    private ChoogooMi ChoogooMi;
 }
