de6c78000a44a4a15b4e87ebabe9059e8fb2d7a9
이학준 <dlgkrwns213@naver.com>
2025-07-31 14:45:10 +0900

    feat: SurveyResponseService 구현

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/service/SurveyResponseService.java
@@ -0,0 +1,23 @@
+package com.choogoomoneyna.choogoomoneyna_be.survey.service;
+
+import com.choogoomoneyna.choogoomoneyna_be.survey.vo.SurveyResponseVO;
+
+import java.util.List;
+
+public interface SurveyResponseService {
+
+    /**
+     * 새로운 설문 응답을 시스템에 삽입합니다.
+     *
+     * @param surveyResponseVO SurveyResponseVO 객체에 포함된 삽입할 설문 응답 데이터
+     */
+    void insertSurveyResponse(SurveyResponseVO surveyResponseVO);
+
+    /**
+     * 제공된 사용자 ID를 기반으로 특정 사용자와 관련된 설문 응답 목록을 조회합니다.
+     *
+     * @param userId 설문 응답을 조회할 사용자의 고유 식별자
+     * @return 지정된 사용자의 설문 응답을 나타내는 SurveyResponseVO 객체 목록
+     */
+    List<SurveyResponseVO> findSurveyResponseByUserId(Long userId);
+}
