0260af8bc2b050b6ce17926ec51c68a6aeb21641
이학준 <dlgkrwns213@naver.com>
2025-07-31 15:36:49 +0900

    feat: DTO 에 값이 제대로 들어오지 않는 경우 에러처리 기능 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/dto/request/SurveyRequestDTO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/dto/request/SurveyRequestDTO.java
@@ -4,19 +4,27 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class SurveyRequestDTO {
-    private List<SingleSurveyAnswerDTO> surveyAnswers;
+    @NotEmpty(message = "설문 응답은 필수입니다.")
+    private List<@Valid SingleSurveyAnswerDTO> surveyAnswers;
 
     @Getter
     @Setter
     @NoArgsConstructor
     public static class SingleSurveyAnswerDTO {
+        @NotNull(message = "질문 ID는 필수입니다.")
         private Integer surveyQuestionId;
+
+        @NotNull(message = "답변 ID는 필수입니다.")
         private Integer surveyAnswerId;
     }
 }
+
