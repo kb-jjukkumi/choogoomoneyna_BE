88876a3a1336de5ebe84f411154cac1b68f31363
이학준 <dlgkrwns213@naver.com>
2025-08-01 10:37:00 +0900

    feat: RankingHistoryDTO 에서 추구미와 라운드 정보 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/dto/response/RankingHistoryDTO.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/ranking/dto/response/RankingHistoryDTO.java
@@ -1,5 +1,6 @@
 package com.choogoomoneyna.choogoomoneyna_be.ranking.dto.response;
 
+import com.choogoomoneyna.choogoomoneyna_be.user.enums.ChoogooMi;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,6 +9,8 @@ import java.util.Date;
 @Getter
 @Builder
 public class RankingHistoryDTO {
+    ChoogooMi choogooMi;
+    Integer roundNumber;
     private Date startDate;
     private Integer ranking;
     private Integer score;
