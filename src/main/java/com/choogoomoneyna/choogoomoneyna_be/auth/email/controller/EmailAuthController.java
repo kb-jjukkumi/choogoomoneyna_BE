ef7ae03a5070b1e8b88ac2575dc996f21f380b42
이학준 <dlgkrwns213@naver.com>
2025-08-01 13:16:11 +0900

    feat: 이메일 체킹 후 이메일 보내는 로직 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/auth/email/controller/EmailAuthController.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/auth/email/controller/EmailAuthController.java
@@ -3,12 +3,11 @@ package com.choogoomoneyna.choogoomoneyna_be.auth.email.controller;
 import com.choogoomoneyna.choogoomoneyna_be.auth.email.dto.EmailAuthRequestDTO;
 import com.choogoomoneyna.choogoomoneyna_be.auth.email.dto.EmailVerifyRequestDTO;
 import com.choogoomoneyna.choogoomoneyna_be.auth.email.service.EmailAuthService;
+import com.choogoomoneyna.choogoomoneyna_be.user.enums.LoginType;
+import com.choogoomoneyna.choogoomoneyna_be.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email-auth")
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailAuthController {
 
     private final EmailAuthService emailAuthService;
+    private final UserService userService;
 
     @PostMapping("/send")
     public ResponseEntity<?> sendAuthCode(@RequestBody EmailAuthRequestDTO dto) {
@@ -29,5 +29,15 @@ public class EmailAuthController {
         return verified ?
                 ResponseEntity.ok("이메일 인증 성공") :
                 ResponseEntity.badRequest().body("이메일 인증 실패");
+    }
+
+    @GetMapping("/password-reset/send")
+    public ResponseEntity<String> checkEmail(@RequestParam String email) {
+        if (userService.existsByEmailAndLoginType(email, LoginType.LOCAL)) {
+            emailAuthService.sendAuthCode(email);
+        }
+        
+        // 보안 상 존재하지 않는 이메일을 위한 메시지는 사용하지 않음
+        return ResponseEntity.ok("인증 코드가 이메일로 전송되었습니다");
     }
 }
