4bb1464233763a243f320f5ea90689373dfadab2
이학준 <dlgkrwns213@naver.com>
2025-08-05 17:15:26 +0900

    feat: GptConfig 선언

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/GptConfig.java
@@ -0,0 +1,37 @@
+package com.choogoomoneyna.choogoomoneyna_be.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+@PropertySource({
+        "classpath:application-secret-openai.properties"
+})
+@Getter
+@Configuration
+public class GptConfig {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
