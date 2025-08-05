b04aa50f7a376f1828846d37929b589ab7aae2b2
권채린 <lin1041840@gmail.com>
2025-08-05 15:33:16 +0900

    feat: 퀴즈 점수를 받아오는 dto 추가

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/dto/Request/MatchingMissionQuizUpdateDTO.java
@@ -0,0 +1,11 @@
+package com.choogoomoneyna.choogoomoneyna_be.matching.dto.Request;
+
+import lombok.Data;
+
+@Data
+public class MatchingMissionQuizUpdateDTO {
+
+    private Integer missionId;
+    private Integer score;
+
+}
