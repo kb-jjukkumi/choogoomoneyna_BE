248020c9ed78f5437c1d885d04239e5f256c46aa
이학준 <dlgkrwns213@naver.com>
2025-08-07 09:39:39 +0900

    feat: 카카오 관련 config 선언

---
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/KakaoOAuthConfig.java
@@ -0,0 +1,36 @@
+package com.choogoomoneyna.choogoomoneyna_be.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource({
+        "classpath:application-secret-kakao.properties"
+})
+@Getter
+@Configuration
+public class KakaoOAuthConfig {
+
+    @Value("${kakao.client-id}")
+    private String clientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${kakao.client-secret}")
+    private String clientSecret;
+
+    @Value("${kakao.auth-uri}")
+    private String authUri;
+
+    @Value("${kakao.token-uri}")
+    private String tokenUri;
+
+    @Value("${kakao.user-info-uri}")
+    private String userInfoUri;
+
+    @Value("${kakao.unlink-uri}")
+    private String unlinkUri;
+}
