ece612fdc57516294ecaa37f851311edb015267b
이학준 <dlgkrwns213@naver.com>
2025-08-08 17:30:02 +0900

    feat: 컴포넌트 스캔 범위를 controller 만 설정

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/RootConfig.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/RootConfig.java
@@ -6,7 +6,17 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan(basePackages = {
-        "com.choogoomoneyna.choogoomoneyna_be"
+        "com.choogoomoneyna.choogoomoneyna_be.account.codef.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.account.db.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.email.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.jwt.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.oauth.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.matching.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.mock.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.ranking.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.report.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.survey.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.user.controller",
 })
 @Configuration
 @Import(MyBatisConfig.class)
