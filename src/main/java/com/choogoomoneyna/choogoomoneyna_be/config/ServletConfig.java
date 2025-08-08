d744400aa98b209d465ef574e6ecd5850c65baf5
이학준 <dlgkrwns213@naver.com>
2025-08-08 17:49:33 +0900

    feat: ComponentScan를 좀더 자세히 써줌

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/ServletConfig.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/config/ServletConfig.java
@@ -1,23 +1,33 @@
 package com.choogoomoneyna.choogoomoneyna_be.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackages = {
+        "com.choogoomoneyna.choogoomoneyna_be.account.codef.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.account.db.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.email.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.jwt.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.auth.oauth.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.matching.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.mock.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.ranking.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.report.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.survey.controller",
+        "com.choogoomoneyna.choogoomoneyna_be.user.controller",
+})
 public class ServletConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-                .resourceChain(false);
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
@@ -29,13 +39,5 @@ public class ServletConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
-    }
-
-    @Bean
-    public UiConfiguration uiConfig() {
-        return UiConfigurationBuilder.builder()
-                .displayRequestDuration(true)
-                .validatorUrl(null)
-                .build();
     }
 }
