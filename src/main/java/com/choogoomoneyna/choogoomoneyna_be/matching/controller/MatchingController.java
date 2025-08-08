60670c40ed72eaeaa60e9e335ba3f901935c9160
이학준 <dlgkrwns213@naver.com>
2025-08-08 16:30:09 +0900

    fix: RestCOntroller 수정

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/controller/MatchingController.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/matching/controller/MatchingController.java
@@ -3,13 +3,13 @@ package com.choogoomoneyna.choogoomoneyna_be.matching.controller;
 import com.choogoomoneyna.choogoomoneyna_be.matching.service.MatchingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/matching")
 public class MatchingController {
