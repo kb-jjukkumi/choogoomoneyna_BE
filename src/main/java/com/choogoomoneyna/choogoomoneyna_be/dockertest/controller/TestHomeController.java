328c8b12b43225b4826bb390eacab65e03f88625
이학준 <dlgkrwns213@naver.com>
2025-08-11 10:32:02 +0900

    fix: Update TestHomeController annotation to @Controller for proper routing

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/dockertest/controller/TestHomeController.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/dockertest/controller/TestHomeController.java
@@ -1,9 +1,10 @@
 package com.choogoomoneyna.choogoomoneyna_be.dockertest.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class TestHomeController {
     @GetMapping("/")
     public String home() {
