fbc8c5a3436a6002423908dac432529f3593e9f4
이학준 <dlgkrwns213@naver.com>
2025-08-13 16:39:07 +0900

    refactor: CustomException 의 생성자 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/CustomException.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/CustomException.java
@@ -16,8 +16,8 @@ public class CustomException extends RuntimeException {
         this.responseCode = responseCode;
     }
 
-    public CustomException(ResponseCode responseCode, Throwable cause) {
-        super(responseCode.getMessage(), cause);
+    public CustomException(ResponseCode responseCode, String customMessage, Throwable cause) {
+        super(customMessage, cause);
         this.responseCode = responseCode;
     }
 }
