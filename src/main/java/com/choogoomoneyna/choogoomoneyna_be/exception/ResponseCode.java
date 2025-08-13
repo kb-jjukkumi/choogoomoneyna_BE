3272e4384c35aba04696170b09561ff03838d2fe
이학준 <dlgkrwns213@naver.com>
2025-08-13 19:10:15 +0900

    feat: ResponseCode에 USER_NOT_FOUND 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/ResponseCode.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/ResponseCode.java
@@ -30,6 +30,7 @@ public enum ResponseCode {
 
     // 404 Not Found
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다."),
     MATCHING_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 매칭입니다."),
     ROUND_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 라운드 정보입니다."),
 
