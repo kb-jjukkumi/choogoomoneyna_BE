a811e168c58064e9a04660d57200ebf54982a6ff
이학준 <dlgkrwns213@naver.com>
2025-08-13 15:20:31 +0900

    refactor: ResponseCode에서 SUCCESS 위치 수정

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/ResponseCode.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/ResponseCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ResponseCode {
 
+    // 200 Success
+    SUCCESS(HttpStatus.OK, "성공했습니다."),
+
     // 400 BadRequest
     AUTH_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "사용자 입력값이 올바르지 않습니다."),
     AUTH_EMAIL_EXISTS(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
@@ -21,9 +24,7 @@ public enum ResponseCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
 
     // 500 Internal Server Error
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."),
-
-    SUCCESS(HttpStatus.OK, "성공했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
