72df4fd3b67200a2cfbdf07706da53223b93bf25
이학준 <dlgkrwns213@naver.com>
2025-08-13 19:07:20 +0900

    feat: SurveyCalculatorServiceImpl에 설문 응답 없을 시 예외 처리 로직 추가

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/service/SurveyCalculatorServiceImpl.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/survey/service/SurveyCalculatorServiceImpl.java
@@ -1,5 +1,7 @@
 package com.choogoomoneyna.choogoomoneyna_be.survey.service;
 
+import com.choogoomoneyna.choogoomoneyna_be.exception.CustomException;
+import com.choogoomoneyna.choogoomoneyna_be.exception.ResponseCode;
 import com.choogoomoneyna.choogoomoneyna_be.survey.vo.SurveyResponseVO;
 import com.choogoomoneyna.choogoomoneyna_be.user.enums.ChoogooMi;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,9 @@ public class SurveyCalculatorServiceImpl implements SurveyCalculatorService {
     @Override
     public ChoogooMi recommendChoogooMi(Long userId) {
         List<SurveyResponseVO> userSurveyResponseList = surveyResponseService.findLatestSurveyResponseByUserId(userId);
+        if (userSurveyResponseList == null || userSurveyResponseList.isEmpty()) {
+            throw new CustomException(ResponseCode.SURVEY_FIND_FAIL);
+        }
         return SurveyWeightCalculator.getTopType(userSurveyResponseList);
     }
 }
