b68e18633c8cc1f943c00f3b2df753a41a3f5c5e
이학준 <dlgkrwns213@naver.com>
2025-08-13 16:27:23 +0900

    refactor: 기존 에러처리가 아닌 CustomException 만 가져옴

--- src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/GlobalExceptionHandler.java
+++ src/main/java/com/choogoomoneyna/choogoomoneyna_be/exception/GlobalExceptionHandler.java
@@ -1,12 +1,9 @@
 package com.choogoomoneyna.choogoomoneyna_be.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.BindException;
 import java.util.Map;
 
 @ControllerAdvice
@@ -21,50 +18,13 @@ public class GlobalExceptionHandler {
                 ));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
-        StringBuilder sb = new StringBuilder();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            sb.append(error.getField())
-                    .append(": ")
-                    .append(error.getDefaultMessage())
-                    .append("; ");
-        });
-        return buildResponse(ResponseCode.AUTH_VALIDATION_ERROR, sb.toString());
-    }
-
-    @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<?> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
-        // 메시지를 Enum 메시지 대신 예외 메시지로 주고 싶으면 ex.getMessage() 사용 가능
-        return buildResponse(ResponseCode.AUTH_EMAIL_EXISTS, ex.getMessage());
-    }
-
-    @ExceptionHandler(BindException.class)
-    @ResponseBody
-    public ResponseEntity<?> handleBindException(BindException ex) {
-        return buildResponse(ResponseCode.BIND_ERROR, "BindException: " + ex.getMessage());
-    }
-
-    @ExceptionHandler(CustomNotFoundException.class)
-    public ResponseEntity<?> handleNotFound(CustomNotFoundException ex) {
-        return buildResponse(ResponseCode.NOT_FOUND, ex.getMessage());
-    }
-
-    @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<?> tokenNotFound(InvalidTokenException ex) {
-        return buildResponse(ResponseCode.AUTH_TOKEN_INVALID, ex.getMessage());
-    }
-
-    @ExceptionHandler(OAuthException.class)
-    public ResponseEntity<?> handleOAuthException(OAuthException ex) {
-        return buildResponse(ResponseCode.OAUTH_FAILURE, ex.getMessage());
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustomException(CustomException ex) {
+        return buildResponse(ex.getResponseCode(), ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     public ResponseEntity<?> handleGeneric(Exception ex) {
-        // 내부 메시지 노출 안 하고 enum 메시지 사용
         return buildResponse(ResponseCode.INTERNAL_SERVER_ERROR, ResponseCode.INTERNAL_SERVER_ERROR.getMessage());
     }
 }
